3894ae0bc9d219fa0fea4178bf76084b8fdc07d2
Daniel Schroeder <deadlydog@hotmail.com>
2018-11-14 00:56:35 -0600

    Introduce aliases for app arguments

--- src/ContinualOutput/ApplicationArgumentsParser.cs
+++ src/ContinualOutput/ApplicationArgumentsParser.cs
@@ -23,13 +23,27 @@ namespace ContinualOutput
 				var nextArgument = (index + 1 < arguments.Length) ? arguments[index + 1] : string.Empty;
 				switch (argument.ToLower())
 				{
-					case "/number": numberOfMessagesToWrite = int.Parse(nextArgument); index++; break;
-					case "/time": numberOfMillisecondsToRunFor = int.Parse(nextArgument); index++; break;
-					case "/writeErrors": shouldWriteErrors = true; break;
-					case "/output": standardOutputString = nextArgument; index++; break;
-					case "/error": standardErrorString = nextArgument; index++; break;
-					case "/exitCode": exitCode = int.Parse(nextArgument); index++; break;
-					case "/delay": delayBetweenMessagesInMilliseconds = int.Parse(nextArgument); index++; break;
+					case "/n":
+					case "/number": numberOfMessagesToWrite = int.Parse(nextArgument); index++;
+						break;
+					case "/t":
+					case "/time": numberOfMillisecondsToRunFor = int.Parse(nextArgument); index++;
+						break;
+					case "/w":
+					case "/writeErrors": shouldWriteErrors = true;
+						break;
+					case "/o":
+					case "/output": standardOutputString = nextArgument; index++;
+						break;
+					case "/e":
+					case "/error": standardErrorString = nextArgument; index++;
+						break;
+					case "/c":
+					case "/exitCode": exitCode = int.Parse(nextArgument); index++;
+						break;
+					case "/d":
+					case "/delay": delayBetweenMessagesInMilliseconds = int.Parse(nextArgument); index++;
+						break;
 					default:
 						Console.Error.WriteLine($"An invalid argument of '{argument}' was provided.");
 						shouldDisplayHelpInstructions = true;
@@ -56,13 +70,14 @@ Usage: ContinualOutput.exe ([/number count] | [/time milliseconds]) [/writeError
 
 You must provide either the /number or /time argument. If both are provided, /time will be ignored.
 
-Options:
-	/number count	The number of output strings to be written before exiting.
-	/time seconds	The number of milliseconds to run for before exiting.
-	/writeErrors	Randomly return a message on the Standard Error stream instead of the Standard Output stream.
-	/output string	The string to write to the Standard Output stream. If not provided a default will be used.
-	/error string	The string to write to the Standard Error stream. If not provided a default will be used.
-	/exitCode int	The number to return when the console app exits. If not provided, 0 will be returned.
+Options (alias first):
+	/n /number count	The number of output strings to be written before exiting.
+	/t /time seconds	The number of milliseconds to run for before exiting.
+	/w /writeErrors		Randomly return a message on the Standard Error stream instead of the Standard Output stream.
+	/o /output string	The string to write to the Standard Output stream. If not provided a default will be used.
+	/e /error string	The string to write to the Standard Error stream. If not provided a default will be used.
+	/c /exitCode int	The number to return when the console app exits. If not provided, 0 will be returned.
+	/d /delay int		The number of milliseconds to wait between output messages.
 ";
 			Console.WriteLine(instructions);
 		}
