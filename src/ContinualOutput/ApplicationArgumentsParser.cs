0339bb6b37394d3a89fe26666680dcc41a21640a
Daniel Schroeder <deadlydog@hotmail.com>
2018-11-21 11:27:53 -0600

    docs: Update indentation of console help parameters

--- src/ContinualOutput/ApplicationArgumentsParser.cs
+++ src/ContinualOutput/ApplicationArgumentsParser.cs
@@ -88,15 +88,15 @@ Usage: ContinualOutput.exe ([/number count] | [/time milliseconds]) [/outputType
 You must provide either the /number or /time argument. If both are provided, /time will be ignored.
 
 Options (alias first):
-	/n /number count	The number of output strings to be written before exiting.
-	/t /time seconds	The number of milliseconds to run for before exiting.
-	/ot /outputType type	Determines if only Standard Output, Standard Error, or both are written. Valid values are 'output', 'error', and 'all'. If not provided, the 'output' will be used.
+	/n /number count		The number of output strings to be written before exiting.
+	/t /time seconds		The number of milliseconds to run for before exiting.
+	/ot /outputType type		Determines if only Standard Output, Standard Error, or both are written. Valid values are 'output', 'error', and 'all'. If not provided, the 'output' will be used.
 	/om /outputMessage message	The string to write to the Standard Output stream. If not provided a default will be used.
 	/em /errorMessage message	The string to write to the Standard Error stream. If not provided a default will be used.
-	/ec /exitCode int	The number to return when the console app exits. If not provided, 0 will be returned. If /throwException is specified, this parameter is ignored.
-	/te /throwException	Throws an exception that causes the application to exit. The exception is thrown after all messages have been written.
+	/ec /exitCode int		The number to return when the console app exits. If not provided, 0 will be returned. If /throwException is specified, this parameter is ignored.
+	/te /throwException		Throws an exception that causes the application to exit. The exception is thrown after all messages have been written.
 	/exm /exceptionMessage message	The string to use for the Exception's message when using the /throwException parameter. If not provided a default will be used.
-	/d /delay int		The number of milliseconds to wait between output messages. If not provided, 1000 will be used.
+	/d /delay int			The number of milliseconds to wait between output messages. If not provided, 1000 will be used.
 ";
 			Console.WriteLine(instructions);
 		}
